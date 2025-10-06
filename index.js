0ccdfa928781a3a23d92eb61ba54b23a6b08ddb1
Manoj <khanavalkarmanoj@gmail.com>
2025-10-06 14:15:40 +0530

    structured the project

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("start of backend Project");
+import dotenv from "dotenv"
+
+dotenv.config({
+    path:"./.env"
+});
+
+let myusername=process.env.user_name;
+
+console.log("Value=",myusername);
+
+console.log("start of backend Project a new journey");
