1bbe1c9bb062dbecd28ffe52bed07ae4162e9899
Manoj <khanavalkarmanoj@gmail.com>
2025-10-13 23:10:15 +0530

    basic validation done

---
+++ src/validators/index.js
@@ -0,0 +1,32 @@
+import { body } from "express-validator";
+
+const userRegisterValidator = () => {
+    return[
+        body("email")
+            .trim()
+            .notEmpty()
+            .withMessage("Email is required")
+            .isEmail()
+            .withMessage("Email is invalid"),
+        body("username")
+            .trim()
+            .notEmpty()
+            .withMessage("username is required")
+            .isLowercase()
+            .withMessage("Username must be lower case")
+            .isLength({min:3})
+            .withMessage("Username must be atleast 3 characters long"),
+        body("password")
+            .trim()
+            .notEmpty()
+            .withMessage("Password is required"),
+        body("fullName")
+            .optional()
+            .trim()
+            .notEmpty()
+            
+    ]
+};
+export{
+    userRegisterValidator
+}
